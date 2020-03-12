d48ecca30dc7dbeea7854afed82a31bc0ce83fd8
jianyan74 <panwy@soe-soe.com>
2020-03-12 15:52:28 +0800

    修复配置文件找不到

---
+++ backend/views/views/console/index.php
@@ -0,0 +1,291 @@
+<?php
+use yii\grid\GridView;
+use common\helpers\Url;
+use common\helpers\Html;
+
+$this->title = '运营中心';
+$this->params['breadcrumbs'][] = $this->title;
+
+$rank = 0;
+?>
+
+<style>
+    .info-box-number {
+        font-size: 22px;
+    }
+
+    .rf-table tbody tr td{
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        word-wrap: normal;
+    }
+</style>
+
+<div class="row">
+    <div class="col-md-2 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <div class="info-box-content p-md">
+                <span class="info-box-number"><i class="ion ion-stats-bars red"></i> <?= $todayData['pay_money'] ?? 0 ?></span>
+                <span class="info-box-text">今日订单总金额(元)</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-2 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <div class="info-box-content p-md">
+                <span class="info-box-number"><i class="icon ion-bag purple"></i> <?= $productCountStat['allCount'] ?></span>
+                <span class="info-box-text">商品发布(个)</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-2 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <div class="info-box-content p-md">
+                <span class="info-box-number"><i class="icon ion-arrow-graph-up-right green"></i> <?= $orderCount ?></span>
+                <span class="info-box-text">订单总数(笔)</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-2 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <div class="info-box-content p-md">
+                <span class="info-box-number"><i class="icon ion-ios-paper-outline cyan"></i> <?= $orderThisMouthStat['count'] ?></span>
+                <span class="info-box-text">本月销量(笔)</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-2 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <div class="info-box-content p-md">
+                <span class="info-box-number"><i class="icon ion-ios-list blue"></i> <?= isset($orderTotalList[4]) ? $orderTotalList[4]['count'] : 0; ?></span>
+                <span class="info-box-text">已完成交易(笔)</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <div class="col-md-2 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <div class="info-box-content p-md">
+                <span class="info-box-number"><i class="icon ion-ios-star-half orange"></i> <?= $productEvaluateCount ?></span>
+                <span class="info-box-text">累计评价(个)</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+</div>
+
+<div class="row shop-index">
+    <div class="col-md-9 col-xs-12">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-circle blue" style="font-size: 8px"></i>
+                <h3 class="box-title">交易提示</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-xs-6 col-md-2 text-center">
+                        <span class="info-box-number blue"><?= isset($orderTotalList[0]) ? $orderTotalList[0]['count'] : 0; ?></span>
+                        <div class="knob-label">待付款</div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-xs-6 col-md-2 text-center">
+                        <span class="info-box-number blue"><?= isset($orderTotalList[1]) ? $orderTotalList[1]['count'] : 0; ?></span>
+                        <div class="knob-label">待发货</div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-xs-6 col-md-2 text-center">
+                        <span class="info-box-number blue"><?= isset($orderTotalList[2]) ? $orderTotalList[2]['count'] : 0; ?></span>
+                        <div class="knob-label">已发货</div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-xs-6 col-md-2 text-center">
+                        <span class="info-box-number blue"><?= isset($orderTotalList[3]) ? $orderTotalList[3]['count'] : 0; ?></span>
+                        <div class="knob-label">已收货</div>
+                    </div>
+                    <div class="col-xs-6 col-md-2 text-center">
+                        <span class="info-box-number blue"><?= isset($orderTotalList[-4]) ? $orderTotalList[-4]['count'] : 0; ?></span>
+                        <div class="knob-label">已关闭</div>
+                    </div>
+                    <div class="col-xs-6 col-md-2 text-center">
+                        <span class="info-box-number blue"><?= isset($orderTotalList[-1]) ? $orderTotalList[-1]['count'] : 0; ?></span>
+                        <div class="knob-label">售后申请</div>
+                    </div>
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <div class="col-md-3 col-xs-12">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-circle blue" style="font-size: 8px"></i>
+                <h3 class="box-title">商品提示</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-xs-6 col-md-4 text-center">
+                        <span class="info-box-number blue"><?= $productCountStat['sellCount'] ?></span>
+                        <div class="knob-label">出售中</div>
+                    </div>
+                    <div class="col-xs-6 col-md-4 text-center">
+                        <span class="info-box-number blue"><?= $productCountStat['warehouseCount'] ?></span>
+                        <div class="knob-label">仓库中</div>
+                    </div>
+                    <div class="col-xs-6 col-md-4 text-center">
+                        <span class="info-box-number blue"><?= $productWarningStockCount ?></span>
+                        <div class="knob-label">库存预警</div>
+                    </div>
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+</div>
+
+<div class="row shop-index">
+    <div class="col-md-9 col-xs-12">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-circle blue" style="font-size: 8px"></i>
+                <h3 class="box-title">订单总数统计</h3>
+            </div>
+            <!-- /.box-header -->
+            <?= \common\widgets\echarts\Echarts::widget([
+                'config' => [
+                    'server' => Url::to(['order-between-count']),
+                    'height' => '220px'
+                ]
+            ])?>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-circle blue" style="font-size: 8px"></i>
+                <h3 class="box-title">销售情况统计</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-xs-12 col-md-12 pull-right" style="padding-top: 31px;padding-bottom: 31px">
+                        <table class="table">
+                            <tbody>
+                            <tr>
+                                <td rowspan="3" align="left">昨日销量</td>
+                            </tr>
+                            <tr>
+                                <td><span>订单量(件)</span></td>
+                                <td><strong><?= $yesterdayData['product_count'] ?? 0 ?></strong></td>
+                            </tr>
+                            <tr>
+                                <td><span>订单金额(元)</span></td>
+                                <td><strong><?= $yesterdayData['pay_money'] ?? 0 ?></strong></td>
+                            </tr>
+                            <tr style="height: 10px;"></tr>
+                            <tr>
+                                <td rowspan="3" align="left">本月销量</td>
+                            </tr>
+                            <tr>
+                                <td><span>订单量(件)</span></td>
+                                <td><strong><?= $orderThisMouthStat['product_count'] ?? 0 ?></strong></td>
+                            </tr>
+                            <tr>
+                                <td><span>订单金额(元)</span></td>
+                                <td><strong><?= $orderThisMouthStat['pay_money'] ?? 0 ?></strong></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <div class="col-md-9 col-xs-12">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-circle blue" style="font-size: 8px"></i>
+                <h3 class="box-title">下单金额</h3>
+            </div>
+            <?= \common\widgets\echarts\Echarts::widget([
+                'config' => [
+                    'server' => Url::to(['sus-res']),
+                    'height' => '315px'
+                ]
+            ])?>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-circle blue" style="font-size: 8px"></i>
+                <h3 class="box-title">单品销售排名</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body" style="height: 375px">
+                <div class="row">
+                    <div class="col-xs-12 col-md-12 pull-right">
+                        <table class="table rf-table">
+                            <thead>
+                            <tr>
+                                <th>排名</th>
+                                <th>商品信息</th>
+                                <th>销量</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($productRank as $item){ ?>
+                            <tr>
+                                <td>
+                                    <?php
+                                    $rank++;
+                                    echo $rank;
+                                    ?>
+                                </td>
+                                <td style="max-width: 100px"><?= $item['name']; ?></td>
+                                <td><?= $item['real_sales']; ?></td>
+                            </tr>
+                            <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+</div>
---
+++ backend/views/views/setting/display.php
@@ -0,0 +1,99 @@
+<?php
+
+use yii\widgets\ActiveForm;
+use common\widgets\webuploader\Files;
+use common\enums\WhetherEnum;
+
+$this->title = '参数设置';
+$this->params['breadcrumbs'][] = ['label' => $this->title];
+?>
+
+<?php $form = ActiveForm::begin([]); ?>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+                <li class="active"><a data-toggle="tab" href="#tab-1" aria-expanded="false">购物设置</a></li>
+                <li><a data-toggle="tab" href="#tab-2" aria-expanded="false">支付设置</a></li>
+                <li><a data-toggle="tab" href="#tab-3" aria-expanded="false">配送设置</a></li>
+                <li><a data-toggle="tab" href="#tab-4" aria-expanded="false">发票设置</a></li>
+                <li><a data-toggle="tab" href="#tab-5" aria-expanded="false">相关协议</a></li>
+                <li><a data-toggle="tab" href="#tab-6" aria-expanded="false">版权</a></li>
+            </ul>
+            <div class="tab-content">
+                <div id="tab-1" class="tab-pane active">
+                    <div class="panel-body">
+                        <?= $form->field($model, 'order_auto_delinery')->textInput(); ?>
+                        <?= $form->field($model, 'order_buy_close_time')->textInput(); ?>
+                        <?= $form->field($model, 'order_delivery_complete_time')->dropDownList([
+                            0 => '立即',
+                            1 => '1天',
+                            2 => '2天',
+                            3 => '3天',
+                            4 => '4天',
+                            5 => '5天',
+                            6 => '6天',
+                            7 => '7天',
+                        ]); ?>
+                        <?= $form->field($model, 'shopping_back_points')->dropDownList([
+                            1 => '订单已完成',
+                            2 => '已收货',
+                            3 => '支付完成',
+                        ]); ?>
+                        <?= $form->field($model, 'evaluate_day')->textInput(); ?>
+                        <?= $form->field($model, 'evaluate')->textarea(); ?>
+                        <?= $form->field($model, 'after_sale_date')->textInput(); ?>
+                    </div>
+                </div>
+                <div id="tab-2" class="tab-pane">
+                    <div class="panel-body">
+                        <?= $form->field($model, 'order_wechat_pay')->radioList(WhetherEnum::getMap()); ?>
+                        <?= $form->field($model, 'order_ali_pay')->radioList(WhetherEnum::getMap()); ?>
+                        <?= $form->field($model, 'order_balance_pay')->radioList(WhetherEnum::getMap()); ?>
+                    </div>
+                </div>
+                <div id="tab-3" class="tab-pane">
+                    <div class="panel-body">
+                        <?= $form->field($model, 'buyer_self_lifting')->radioList(WhetherEnum::getMap()); ?>
+                        <?= $form->field($model, 'is_logistics')->radioList(WhetherEnum::getMap()); ?>
+                    </div>
+                </div>
+                <div id="tab-4" class="tab-pane">
+                    <div class="panel-body">
+                        <?= $form->field($model, 'order_invoice_tax')->textInput(); ?>
+                        <?= $form->field($model, 'order_invoice_content')->textarea(); ?>
+                    </div>
+                </div>
+                <div id="tab-5" class="tab-pane">
+                    <div class="panel-body">
+                        <?= $form->field($model, 'protocol_register')->widget(\common\widgets\ueditor\UEditor::class); ?>
+                        <?= $form->field($model, 'protocol_privacy')->widget(\common\widgets\ueditor\UEditor::class); ?>
+                        <?= $form->field($model, 'protocol_recharge')->widget(\common\widgets\ueditor\UEditor::class); ?>
+                    </div>
+                </div>
+                <div id="tab-6" class="tab-pane">
+                    <div class="panel-body">
+                        <?= $form->field($model, 'copyright_logo')->widget(Files::class, [
+                            'type' => 'images',
+                            'theme' => 'default',
+                            'themeConfig' => [],
+                            'config' => [
+                                'pick' => [
+                                    'multiple' => false,
+                                ],
+                            ],
+                        ]); ?>
+                        <?= $form->field($model, 'copyright_companyname')->textInput(); ?>
+                        <?= $form->field($model, 'copyright_url')->textInput(); ?>
+                        <?= $form->field($model, 'copyright_desc')->textarea(); ?>
+                    </div>
+                </div>
+                <div class="box-footer text-center">
+                    <button class="btn btn-primary" type="submit">保存</button>
+                    <span class="btn btn-white" onclick="history.go(-1)">返回</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php ActiveForm::end(); ?>
