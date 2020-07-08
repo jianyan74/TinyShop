04c2de614f71ffa3aca6856cd1d7d7ab021368b9
jianyan74 <panwy@soe-soe.com>
2020-07-08 15:28:50 +0800

    修改卸载文件

--- UnInstall.php
+++ UnInstall.php
@@ -11,7 +11,8 @@ use common\interfaces\AddonWidget;
  * 卸载
  *
  * Class UnInstall
- * @package addons\TinyShop */
+ * @package addons\TinyShop
+ */
 class UnInstall extends Migration implements AddonWidget
 {
     /**
@@ -23,8 +24,8 @@ class UnInstall extends Migration implements AddonWidget
     */
     public function run($addon)
     {
-        // MigrateHelper::downByPath([
-        //     '@addons/TinyShop/console/migrations/'
-        // ]);
+         MigrateHelper::downByPath([
+             '@addons/TinyShop/console/migrations/'
+         ]);
     }
 }
